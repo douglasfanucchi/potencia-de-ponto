d8db41cc1ddf0890858ad912b0b0d299d5e39c66
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-05-18 16:06:11 -0300

    changes manim lib to community version and implements FilledAngle class

--- start.py
+++ start.py
@@ -1,44 +1,46 @@
-from manimlib import *
+from manim import *
 import numpy as np
+
+class FilledAngle(VMobject):
+    def __init__(
+            self,
+            l1: Line,
+            l2: Line,
+            radius: float,
+            color = BLUE,
+            opacity = 0.5,
+            other_angle = True,
+            **kwargs):
+        super().__init__()
+        a1 = Angle(l1, l2, other_angle=other_angle, radius=0).set_color(color)
+        a2 = Angle(l1, l2, other_angle=other_angle, radius=radius).set_color(color)
+        q1 = a1.points
+        q2 = a2.reverse_direction().points
+        pnts = np.concatenate([q1, q2, q1[0].reshape(1, 3)])
+        self.set_color(color)
+        self.set_points_as_corners(pnts).set_fill(color, opacity)
 
 class PotenciaDePonto(Scene):
     def construct(self):
         centered_circle = Circle(radius=2.5)
-        centered_circle.set_stroke(WHITE, width=1)
+        centered_circle.set_stroke(WHITE, width=3)
 
-        P = SmallDot(
-            point=np.array([-5, 2, 0])
-        )
+        P = Dot(
+		point=np.array([-5, 2, 0]),
+		radius=0.03
+	)
 
         pLabel = Text("P", font_size=14)
         pLabel.move_to(P.get_points()[0] + np.array([-0.1, 0.1, 0]))
 
-        A = Line(start=P.get_points()[0], end=np.array([2.3, 1]))
-        A.set_stroke(WHITE, width=1)
+        A = Line(start=P.get_points()[0], end=np.array([2.3, 1, 0]))
+        A.set_stroke(WHITE, width=3)
 
-        B = Line(start=P.get_points()[0], end=np.array([2.3, -1]))
-        B.set_stroke(WHITE, width=1)
+        B = Line(start=P.get_points()[0], end=np.array([2.3, -1, 0]))
+       	B.set_stroke(WHITE, width=3)
 
-        arc = ArcBetweenPoints(
-            start=P.get_points()[0] + B.get_unit_vector(),
-            end=P.get_points()[0] + A.get_unit_vector(),
-            stroke_width=1
-        )
+        arc = FilledAngle(A, B, 5)
 
-        radius = get_norm(P.get_points()[0] - arc.get_start())
-        angle = (arc.get_arc_length() / radius) * 0.9
-        sector = Sector(
-            angle=angle,
-            radius=radius,
-            arc_center=P.get_points()[0],
-            start_angle=B.get_angle(),
-            fill_color=BLUE,
-            fill_opacity=0.5
-        )
-
-        self.play(ShowCreation(P))
-        self.play(Write(pLabel))
-        self.play(ShowCreation(centered_circle))
-        self.play(ShowCreation(A))
-        self.play(ShowCreation(B))
-        self.play(ShowCreation(sector))
+        self.play(Create(arc))
+        self.play(Write(pLabel), Create(P), Create(centered_circle))
+        self.play(Create(A), Create(B))
