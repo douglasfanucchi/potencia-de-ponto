fd3c7e6e00d6b2ac951c639e6f6b1c97513614b6
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-05-19 20:30:18 -0300

    improves code flow

--- start.py
+++ start.py
@@ -43,22 +43,21 @@ class PotenciaDePonto(Scene):
                 )
             )
 
-        l1 = Line(start=P.dot.points[0], end=np.array([2.3, 1, 0]))
-        l1.set_stroke(WHITE, width=3)
-        l2 = Line(start=P.dot.points[0], end=np.array([0, -2.5, 0]))
-       	l2.set_stroke(WHITE, width=3)
-        arc = FilledAngle(l1, l2, 1, other_angle=True)
-        l1_intersec = self.circle_intersection(l1, centered_circle)
-        l2_intersec = self.circle_intersection(l2, centered_circle)
-        l3 = Line(start=l1_intersec[1], end=l2_intersec[0])
-        l4 = Line(start=l2_intersec[1], end=l1_intersec[0])
-
-        arc2 = FilledAngle(l1, l3, 1, color=ORANGE, quadrant=np.array([-1, 1]))
-        arc3 = FilledAngle(l4, l2, 1, color=ORANGE, quadrant=np.array([1, -1]))
-
+        l1 = Line(start=P.dot.points[0], end=np.array([2.3, 1, 0]), stroke_width=3)
+        l2 = Line(start=P.dot.points[0], end=np.array([0, -2.5, 0]), stroke_width=3)
         self.play(Write(P.label), Create(P.dot), Create(centered_circle))
         self.play(Create(l1), Create(l2))
+
+        l1_intersec = self.circle_intersection(l1, centered_circle)
+        l2_intersec = self.circle_intersection(l2, centered_circle)
+
+        l3 = Line(start=l1_intersec[1], end=l2_intersec[0])
+        l4 = Line(start=l2_intersec[1], end=l1_intersec[0])
         self.play(Create(l3), Create(l4))
+
+        arc = FilledAngle(l1, l2, 1, other_angle=True)
+        arc2 = FilledAngle(l1, l3, 1, color=ORANGE, quadrant=np.array([-1, 1]))
+        arc3 = FilledAngle(l4, l2, 1, color=ORANGE, quadrant=np.array([1, -1]))
         self.play(Create(arc), Create(arc2), Create(arc3))
         self.wait(5)
 
